348e2567bc6ad728fdb6cf8e7aa3af3da447aeb3
DevShubam <65375709+DevShubam@users.noreply.github.com>
2022-11-06 11:10:21 -0500

    Update temp-converter.js

--- temperature/temp-converter.js
+++ temperature/temp-converter.js
@@ -1,13 +1,7 @@
-
-var temp = {
-    
-    'EUR': {'PLN': 4.15, 'USD': 0.83, 'CAD': 1.57 }, 
-    'USD': {'PLN': 3.45, 'EUR': 1.2, 'CAD': 1.28}, 
-    'CAD': {'PLN': 2.85, 'USD': 0.78, 'EUR': 0.64 }, 
-    'PLN': {'EUR': 0.22, 'CAD': 0.35, 'USD': 0.27}
-  
-  
-}
+var crrncy = {'Celcius': {'Kelvin': 274.15, 'Farenheit': 33.8}, 
+            'Kelvin': {'Celcius': -272.15, 'Farenheit': -457.87},
+            'Farenheit': {'Celcius': -17.2222, 'Kelvin': 255.95}
+             }
 var btn = document.querySelector('.calculate-btn');
 var baseCurrencyInput = document.getElementById('currency-1');
 var secondCurrencyInput = document.getElementById('currency-2');
@@ -18,27 +12,27 @@ var toShowSecond = document.querySelector('.second-currency');
 var toShowResult = document.querySelector('.final-result');
 
 function convertCurrency(event) {
-event.preventDefault();
-var amount = amountInput.value;
-var from = baseCurrencyInput.value;
-var to = secondCurrencyInput.value;
-var result = 0;
-
-try{
-if (from == to){
-result = amount;
-} else {
-result = amount * temp[from][to];
-}
-}
-catch(err) {
-result = amount * (1 / temp[to][from]);
-}
-
-toShowAmount.innerHTML = amount;
-toShowBase.textContent = from + ' = ';
-toShowSecond.textContent = to;
-toShowResult.textContent = result; 
+  event.preventDefault();
+  var amount = amountInput.value;
+  var from = baseCurrencyInput.value;
+  var to = secondCurrencyInput.value;
+  var result = 0;
+  
+  try{
+    if (from == to){
+      result = amount;
+    } else {
+     result = amount * crrncy[from][to];
+  }
+  }
+  catch(err) {
+    result = amount * (1 / crrncy[to][from]);
+  }
+  
+  toShowAmount.innerHTML = amount;
+  toShowBase.textContent = from + ' = ';
+  toShowSecond.textContent = to;
+  toShowResult.textContent = result; 
 }
 
 btn.addEventListener('click', convertCurrency);

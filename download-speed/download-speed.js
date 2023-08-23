f87acdb9aa92207e91bbefc72584eba3161dd774
Shubam <65375709+DevShubam@users.noreply.github.com>
2023-08-23 15:32:33 -0400

    update

--- download-speed/download-speed.js
+++ download-speed/download-speed.js
@@ -1,3 +1,8 @@
 function calculateDownloadTime() {
+    // get input values
+    const downloadSpeed = document.getElementById("download-speed").value;
+    const fileSize = document.getElementById("file-size").value;
+    const fileSizeUnit = document.getElementById("fileSizeUnit").value;
+    const downloadSpeedUnit = document.getElementById("downloadSpeedUnit").value;
 
 }
